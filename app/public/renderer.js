b7593840878696137060a27db923adb6eb7ab309
Saket Satta <saket.satta@autodesk.com>
2019-09-11 18:17:52 -0700

    Some clean-up

--- app/public/renderer.js
+++ app/public/renderer.js
@@ -3,10 +3,27 @@
 // All of the Node.js APIs are available in this process.
 
 //import sumAll from '../src/js/musat_grid_graph.js';
+
 let $ = require('jquery');
-let count = 0
+
+var ary = text.split("\n");
+var muFullData = JSON.parse(data);
+mu = {};
+mu.oLocusCalls = muFullData["oLocusCalls"];
+hits = {};
+orig_hits = {};
+pop_info = {};
+names = [];
+pts_merged = {};
+num_pops = 0;
+initPrimerMinMax(); // 10May2016
+sample_pts_trimmed = {}; // 10May2016 each sample has .left and .right arrays to show any pt trimmed when having too few reads to 
+processData(ary);
+new GridLocus("TG_MS1", hits, primer_min, primer_max, names, "TG_MS1");
+
+/*let count = 0
 $('#click-counter').text(count.toString())
 $('#countbtn').on('click', () => {
    count ++ 
    $('#click-counter').text(count)
-}) 
+}) */

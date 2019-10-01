fcb9649dceb448b7b219deaffd261286ee7a37cb
Charlie G Zhang <charlieg.zhang@autodesk.com>
2019-10-01 15:18:32 -0700

    Move sample data sets in sampleData folder

--- app/src/Components/App/Body/Graph/index.js
+++ app/src/Components/App/Body/Graph/index.js
@@ -6,8 +6,12 @@ import { g, hits, names, mu } from "../../../../modules/musat_global"
 import { initPrimerMinMax } from "../../../../modules/musat_global"
 import { processData } from "../../../../modules/musat_process_graph_data"
 import GridLocus from "../../../../modules/musat_grid_locus"
-import * as data from "../../muinfo.json"
-import text from "../../TG_MS1_AllelCall"
+
+import text from "../../../../sampleData/TG_MS1_AllelCall"
+import * as data from "../../../../sampleData/muinfo.json"
+//import text from "../../../../sampleData/test1/mly-tavg-stddev.js"
+//import * as data from "../../../../sampleData/test1/muinfo.json"
+
 import './index.css'
 
 class Graph extends React.Component {
@@ -27,7 +31,13 @@ class Graph extends React.Component {
     }
 
     componentDidMount() {
+        window.alert("begin componentDidMount")
         const fileName = this.state.graphData.fileName
+        console.log("fileName", fileName)
+        console.log("hits", hits)
+        console.log("g", g)
+        console.log("names", names)
+        console.log("mu", mu)
         new GridLocus(
             fileName,
             hits,

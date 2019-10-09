8b3b48ae3d31c6b5663e4d45ef48f0f78ef94350
Saket Satta <saket.satta@autodesk.com>
2019-10-08 17:59:27 -0700

    Styling of the graph menu buttons

--- src/Components/App/Body/Graph/Buttons/index.js
+++ src/Components/App/Body/Graph/Buttons/index.js
@@ -2,10 +2,15 @@ import React from 'react'
 import './index.css'
 
 let Buttons = (props) => (
-    <div id="buttons" className="buttons">
-        <button id="addGroup" onClick={props.graphActions.addGroup}>Add group</button>
-        <button id="removeGroup" onClick={props.graphActions.removeGroup}>Remove group</button>
-        <div id="gridGraphSelector" className="plot-grid-selector" />
+    <div id="buttons" className="graph-menu">
+        <ul className="graph-menu-list">
+            <li  className="graph-menu-item" id="addGroup" onClick={props.graphActions.addGroup}>Add group</li>
+            <li  className="graph-menu-item" id="removeGroup" onClick={props.graphActions.removeGroup}>Remove group</li>
+            <li  className="graph-menu-item">
+                <div id="gridGraphSelector" className="plot-grid-selector" />
+            </li>
+        </ul>
+        
     </div>
 )
 
